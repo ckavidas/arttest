849346d6691bf9a78c11baaf4ad19076e5637f07
ckavidas <49797382+ckavidas@users.noreply.github.com>
2022-04-02 15:39:55 -0400

    Create download_test.py

---
+++ pipeline/py/download_test.py
@@ -0,0 +1,31 @@
+import requests as r
+
+info = {
+  repo: "arttest",
+  user: "ckavidas",
+  headers: {"Accept":"application/vnd.github.v3+json"},
+  baseurl: "https://api.github.com/repos",
+}
+
+def get_artifacts_list(info):
+  uri = "{baseurl}/{user}/{repo}/actions/artifacts".format(
+    baseurl=info['baseurl'],
+    user=info['user'],
+    repo=info['repo'],
+  )
+  req = r.get(uri, headers=info['headers'])
+  results = {
+    "Status_Code": req.status_code,
+    "JSON": req.json(),
+  }
+  return results
+
+def main():
+  info = {
+    repo: "arttest",
+    user: "ckavidas",
+    headers: {"Accept":"application/vnd.github.v3+json"},
+    baseurl: "https://api.github.com/repos",
+  }
+  test = get_artifacts_list(info)
+  print(test)
